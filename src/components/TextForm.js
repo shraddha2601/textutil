cbb01027c5e3acab25e83950c5a01384948e85c1
shraddha gediya <rupesh.halpati@aspiresoftserv.com>
2022-07-19 18:38:07 +0530

    work on props concept

---
+++ src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    <div>
+        <div class="form-group">
+            <label htmlFor="myBox">Example textarea</label>
+            <textarea class="form-control" id="myBox" rows="3"></textarea>
+        </div>
+    </div>
+  )
+}
